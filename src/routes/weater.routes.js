6103e7b3e871499f0d58d3acd65b6765706b4312
Francisco Jimenez <francisco.jimenez@naranjax.com>
2021-09-03 18:12:41 -0300

    remove: home

--- src/routes/weater.routes.js
+++ src/routes/weater.routes.js
@@ -1,12 +1,10 @@
-const express = require('express')
-const controller = require('../controller')
-const routes = express.Router()
+const express = require("express");
+const controller = require("../controller");
+const routes = express.Router();
 
-routes.get('/',controller.weather.home)
-      .get('/location',controller.weather.location)
-      .get('/current/:city?',controller.weather.current)
-      .get('/forecast/:city?',controller.weather.forecast)
+routes
+  .get("/location", controller.weather.location)
+  .get("/current/:city?", controller.weather.current)
+  .get("/forecast/:city?", controller.weather.forecast);
 
-module.exports = routes
-
-
+module.exports = routes;
